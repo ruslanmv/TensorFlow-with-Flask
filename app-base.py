34c8c9d9302aa0438ed314675635780961a1c5c2
Your Name <ruslan_mv@mail.ru>
2020-09-11 21:09:21 +0000

    First Commit

---
+++ app-base.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+"""
+Routes
+"""
+@app.route('/',methods=['GET'])
+def index():
+    return "Hello World!"
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
