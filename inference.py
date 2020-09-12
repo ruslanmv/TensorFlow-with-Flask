dce1f64698f6e4ef3ec7d474a1984cc1518e9eb1
Your Name <ruslan_mv@mail.ru>
2020-09-12 10:10:57 +0000

    update

---
+++ inference.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+import numpy as np
+import json
+import requests
+
+SIZE=128
+MODEL_URI='http://localhost:8501/v1/models/pets:predict'
+
+def get_prediction(image_path):
+    image = tf.keras.preprocessing.image.load_img(
+       image_path, target_size=(SIZE, SIZE)
+       )
+     image = tf.keras.preprocessing.image.img_to_array(image)
+     image = tf.keras.applications.mobilenet_v2.preprocess_input(image)
+     image = np.expand_dims(image, axis=0)
+
+     data = json.dumps({
+          'instances': image.tolist()
+     })
+     response = requests.post(MODEL_URI, data=data.encode())
+     result = json.loads(response.text)
+     print(result)
+
+get_prediction('dog.jpg')
---
+++ step2.sh
@@ -0,0 +1,1 @@
+sudo docker run -p 8501:8501 --name=pets -v "/home/rhyme/Desktop/app/pets/:/models/pets/1" -e MODEL_NAME=pets tensorflow/serving
