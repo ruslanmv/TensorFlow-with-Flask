6f69d1c6534eb7a75e45057fdbddfcfe1afd2053
Your Name <ruslan_mv@mail.ru>
2020-09-12 10:18:13 +0000

    update

--- inference.py
+++ inference.py
@@ -9,16 +9,16 @@ MODEL_URI='http://localhost:8501/v1/models/pets:predict'
 def get_prediction(image_path):
     image = tf.keras.preprocessing.image.load_img(
        image_path, target_size=(SIZE, SIZE)
-       )
-     image = tf.keras.preprocessing.image.img_to_array(image)
-     image = tf.keras.applications.mobilenet_v2.preprocess_input(image)
-     image = np.expand_dims(image, axis=0)
+    )
+    image = tf.keras.preprocessing.image.img_to_array(image)
+    image = tf.keras.applications.mobilenet_v2.preprocess_input(image)
+    image = np.expand_dims(image, axis=0)
 
-     data = json.dumps({
+    data = json.dumps({
           'instances': image.tolist()
-     })
-     response = requests.post(MODEL_URI, data=data.encode())
-     result = json.loads(response.text)
-     print(result)
+    })
+    response = requests.post(MODEL_URI, data=data.encode())
+    result = json.loads(response.text)
+    print(result)
 
 get_prediction('dog.jpg')
